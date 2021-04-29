f5b18a9c7eb4bf0281a366f9f9ab30df93fb09f3
Ihor Palii <ithor@communityconnectlabs.com>
2021-04-29 15:53:17 +0300

    Removed data

--- bot/views.py
+++ bot/views.py
@@ -7,6 +7,6 @@ from .models import MessageUpdate
 class WebhookListener(View):
     def post(self, request: HttpRequest, *args, **kwargs):
         body_data = request.POST.dict()
-        body_data.update(request.GET.dict())
+        print(body_data)
         update = MessageUpdate.objects.create(token=kwargs.get("token", ""), data=body_data)
         return JsonResponse({"update_id": update.id})
